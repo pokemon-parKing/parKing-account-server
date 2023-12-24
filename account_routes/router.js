fed1883d12ce788a4f294e2fe68cc3bed0a2b99c
bzw24 <brucewong24@gmail.com>
2023-12-23 16:36:16 -0800

    edited folder structures

--- account_routes/router.js
+++ account_routes/router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const controllers = require('./controllers');
+const controllers = require('../account_controllers/controllers');
 require('dotenv').config();
 
 router.route('/user').get(controllers.accountInfo.getUsersData);
