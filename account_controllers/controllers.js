5d008e30f46eeb7f6a722afa6da69ad8a0073cd0
bzw24 <brucewong24@gmail.com>
2023-12-25 18:42:06 -0800

    completed backend service for accounts and valet

--- account_controllers/controllers.js
+++ account_controllers/controllers.js
@@ -80,5 +80,139 @@ module.exports = {
         res.status(500).send('Internal Server Error');
       }
     },
+
+    addVehicle: async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { make, model, color, license_plate_number } = req.body;
+        const { data: userData, error: userError } = await supabase
+          .from('accounts')
+          .select('id')
+          .eq('id', id)
+          .single();
+        if (userError) {
+          console.error('Error checking user existence:', userError);
+          return res.status(500).send('Internal Server Error');
+        }
+        if (!userData) {
+          return res.status(404).send('User not found');
+        }
+        const { data, error } = await supabase.from('cars').insert([
+          {
+            make,
+            model,
+            color,
+            license_plate_number,
+            user_id: id,
+          },
+        ]);
+        if (error) {
+          console.error('Error adding vehicle:', error);
+          return res.status(500).send('Internal Server Error');
+        }
+        res.status(201).send('Successfully added vehicle');
+      } catch (error) {
+        console.error('Error adding vehicle:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    },
+
+    getCurrentReservations: async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { data: reservationsData, error: reservationsError } =
+          await supabase
+            .from('reservations')
+            .select(
+              `
+            parking_spot_id,
+            date,
+            time,
+            garages ( name, address, city, state, country, zip)
+          `
+            )
+            .eq('user_id', id);
+        if (reservationsError) {
+          console.error('Error fetching reservations:', reservationsError);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        if (!reservationsData || reservationsData.length === 0) {
+          return res.status(404).json({ error: 'No reservations found' });
+        }
+        res.status(200).json({ reservations: reservationsData });
+      } catch (error) {
+        console.error('Error fetching reservations:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    },
+  },
+
+  valetInfo: {
+    getValetData: async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { data, error } = await supabase
+          .from('garages')
+          .select('*')
+          .eq('user_id', id);
+        if (error) {
+          console.error('Error fetching valet data:', error);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        if (!data || data.length === 0) {
+          return res
+            .status(404)
+            .json({ error: 'No garages found for the given user' });
+        }
+        res.status(200).json({ garages: data });
+      } catch (error) {
+        console.error('Error fetching valet data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    },
+
+    editOperationHours: async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { operation_hours } = req.body;
+        const { data, error } = await supabase
+          .from('garages')
+          .update({ operation_hours })
+          .eq('user_id', id)
+          .single();
+        if (error) {
+          console.error('Error updating operation hours:', error);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        res
+          .status(200)
+          .json({ message: 'Operation hours updated successfully' });
+      } catch (error) {
+        console.error('Error updating operation hours:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    },
+
+    editGarageParkingSpots: async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { spots } = req.body;
+        const { data, error } = await supabase
+          .from('garages')
+          .update({ spots })
+          .eq('user_id', id)
+          .single();
+        if (error) {
+          console.error('Error updating garage parking spots:', error);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        res
+          .status(200)
+          .json({ message: 'Garage parking spots updated successfully' });
+      } catch (error) {
+        console.error('Error updating garage parking spots:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    },
   },
 };
--- account_routes/router.js
+++ account_routes/router.js
@@ -2,11 +2,24 @@ const router = require('express').Router();
 const controllers = require('../account_controllers/controllers');
 require('dotenv').config();
 
+// User Routes
 router
   .route('/user/:id')
   .get(controllers.accountInfo.getUserData)
   .put(controllers.accountInfo.updateUserData);
-
 router.route('/user/:id/cars').get(controllers.accountInfo.getUserCarData);
+router.route('/user/:id/add-vehicle').post(controllers.accountInfo.addVehicle);
+router
+  .route('/user/:id/current-reservations')
+  .get(controllers.accountInfo.getCurrentReservations);
+
+// Valet Routes
+router.route('/valet/:id').get(controllers.valetInfo.getValetData);
+router
+  .route('/valet/:id/operation-hours')
+  .put(controllers.valetInfo.editOperationHours);
+router
+  .route('/valet/:id/spots')
+  .put(controllers.valetInfo.editGarageParkingSpots);
 
 module.exports = router;
